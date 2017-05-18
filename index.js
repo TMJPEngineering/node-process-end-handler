8c68f2c19a934a75a438f151b4deb75acdc1f150
rejtg21 <rejmediodia@Rejs-iMac.local>
2017-05-18 14:12:37 +0800

    added process handler for sudo kill

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * @author TMJP ENGINEERING
  * @copyright 2017
  * reference http://stackoverflow.com/questions/18692536/node-js-server-close-event-doesnt-appear-to-fire
+ * , http://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+ * http://joseoncode.com/2014/07/21/graceful-shutdown-in-node-dot-js/
  */
 
 module.exports = ProcessEndHandler();
@@ -28,13 +30,37 @@ function ProcessEndHandler() {
     }
 
     function initialize() {
-        // listen for close process
-        process.on('SIGINT', function() {
+        // since we only want to run the call back once on any event that triggers
+        let check = 0;
+
+        // prevent the app to close instantly
+        process.stdin.resume();
+
+        // listen for close processes
+
+        // on ctrl + c
+        process.on('SIGINT', onEnd);
+        // on app close
+        process.on('exit', onEnd);
+        // on uncaught exceptions
+        process.on('uncaughtException', onEnd)
+        // on process terminated/kill
+        process.on('SIGTERM', onEnd);
+
+        /**
+         * if uncaught exception error has a value
+         */
+        function onEnd(err) {
+            if (check) return;
+            // if it is not yet run increment the checker to inform that this was already run
+            check++;
+
             // run process before closing
             console.log('Before process end, running all registered callbacks');
-            runCallbacks();
+            runCallbacks(err);
             console.log('All registered callbacks have been run, process will now end ...');
+            // inform to continue to exit
             process.exit(0);
-        });
+        }
     }
 }
